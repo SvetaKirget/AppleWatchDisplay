e3fdc4e59f77e6f23edebd5b597785eddb2be0c0
SvetaKirget <kirgetsveta@gmail.com>
2022-06-20 16:01:06 +0300

    add clock and year

---
+++ js/app.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+  window.setInterval(function () {
+    let date = new Date();
+
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let year = date.getFullYear();
+
+    if (hours < 10) hours = `0` + hours;
+    if (minutes < 10) minutes = `0` + minutes;
+    let clock = hours + `:` + minutes;
+    document.getElementById(`clock`).innerHTML = clock;
+    document.getElementById(`year`).innerHTML = year;
+  });
+};
