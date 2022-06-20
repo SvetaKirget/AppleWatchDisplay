b64591fcdf87c9aa13e21f5555c210f87fe52b88
SvetaKirget <kirgetsveta@gmail.com>
2022-06-20 17:14:04 +0300

    add changes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
-window.onload = function () {
-  window.setInterval(function () {
-    let date = new Date();
+function createClock() {
+  let date = new Date();
 
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let year = date.getFullYear();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let year = date.getFullYear();
 
-    if (hours < 10) hours = `0` + hours;
-    if (minutes < 10) minutes = `0` + minutes;
-    let clock = hours + `:` + minutes;
-    document.getElementById(`clock`).innerHTML = clock;
-    document.getElementById(`year`).innerHTML = year;
-  });
+  if (hours < 10) hours = `0` + hours;
+  if (minutes < 10) minutes = `0` + minutes;
+  let clock = hours + `:` + minutes;
+  document.getElementById(`clock`).innerHTML = clock;
+  document.getElementById(`year`).innerHTML = year;
+  setInterval(createClock(), 1000);
 };
+
+createClock();
